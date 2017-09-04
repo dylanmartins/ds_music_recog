4cc50ef5b921277a3b4ff01786b6626f19609edf
dylanlinux <andrademdylan@gmail.com>
2017-09-04 03:26:56 -0300

    Implementação de mais testes

--- artificial_inteligence/test2.py
+++ artificial_inteligence/test2.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 import os
+from collections import Counter
+import numpy as np
 import re
 import pickle
 import pandas as pd
@@ -13,12 +15,18 @@ from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.cross_validation import train_test_split
 
-stopwords_ = nltk.corpus.stopwords.words('portuguese') + [u'é', u'pra', u'vai', u'vou', u'tá', u'vem', u'amor',
+stopwords_ = nltk.corpus.stopwords.words('portuguese') + [u'é', u'pra', u'amor', u'vai', u'vou', u'tá', u'vem',
                                                           u'hoje', u'to', u'quero', u'ai', u'gente', u'tô', u'sei']
 regex = re.compile('[^a-zA-Z]')
 repetitions_regex = re.compile('(\[x\d+\])')
 repetitions_regex2 = re.compile('(\(x\d+\))')
 path = os.getcwd() + '/files/lyrics/'
+
+sertanejo_counts = Counter()
+funk_counts = Counter()
+gospel_counts = Counter()
+bossa_counts = Counter()
+total_counts = Counter()
 
 
 def clean_data(text):
@@ -32,7 +40,8 @@ def clean_data(text):
             text = text.replace(r, ' ')
     return text.replace('(', '').replace(')', '')
 
-dados, y = [], []
+
+dados, labels = [], []
 print('Pegando os dados ...')
 for csv in os.listdir(path):
     # Pegando os dados
@@ -40,7 +49,7 @@ for csv in os.listdir(path):
     data = pd.read_csv(path+csv)
     for x in data['lyric']:
         dados.append(x)
-        y.append(label)
+        labels.append(label)
 
 corpus = []
 print('Limpando dados ...')
@@ -52,34 +61,22 @@ for dado in dados:
     teste = ' '.join([x for x in teste.split() if x not in set(stopwords_)])
     corpus.append(teste)
 
-# Separando teste e treino
-# 80% para treino e 20% para testes
-X_train, X_test, y_train, y_test = train_test_split(corpus, y, test_size=0.20, random_state=0)
+for i in range(len(corpus)):
+    if labels[i] == 'SERTANEJO':
+        for word in corpus[i].split(" "):
+            sertanejo_counts[word] += 1
+            total_counts[word] += 1
+    elif labels[i] == 'FUNK':
+        for word in corpus[i].split(" "):
+            funk_counts[word] += 1
+            total_counts[word] += 1
+    elif labels[i] == 'GOSPEL':
+        for word in corpus[i].split(" "):
+            gospel_counts[word] += 1
+            total_counts[word] += 1
+    else:
+        for word in corpus[i].split(" "):
+            bossa_counts[word] += 1
+            total_counts[word] += 1
 
-best_score = 0.000000000001
-epochs = 500
-for x in range(epochs):
-    c = uniform(0, 10)
-    # c = 10.0
-    # Utilizei pipeline para juntar os passos da transformação que podem ser validadas em conjunto
-    pipeline = make_pipeline(
-        CountVectorizer(),
-        TfidfTransformer(norm='l2', use_idf=False, sublinear_tf=True),
-        LogisticRegression(C=c)#, multi_class='multinomial', solver='lbfgs')
-    )
-    pipeline.fit(X_train, y_train)
-    score = pipeline.score(X_test, y_test)
-    y_pred = pipeline.predict(X_test)
-    if score > best_score:
-        print('Epoch {0} - C = {1} score {2}'.format(x, c, str(score)))
-        print('Best one epoch {0} with C = {1}!!!!!!! Saved.'.format(x, c))
-        best_score = score
-        # Salva modelo
-        filename = 'song_recog_model.sav'
-        pickle.dump(pipeline, open(filename, 'wb'))# precisão 0.8515625
-        print('-' * 80)
-
-# y_pred = pipeline.predict(X_test)
-# cm = confusion_matrix(y_test, y_pred)
 print('FIM')
-
